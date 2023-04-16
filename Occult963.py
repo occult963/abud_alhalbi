e43df3d365ea3657570299fc79813912ba59b9df
occult963 <130882515+occult963@users.noreply.github.com>
2023-04-16 06:45:17 +0300

    Update and rename README.md to Occult963.py

---
+++ Occult963.py
@@ -0,0 +1,27 @@
+
+equest URL: https://ar-ar.facebook.com/ajax/bz?__a=1&__ccg=EXCELLENT&__comet_req=0&__dyn=7xe6E5aQ1PyUbFuC1swgE98nwgU29zEdEc8uwdK0lW4o3Bw5VCwjE3awbG782Cw8G0um4o5-0me2218w5uw5Uwdq0Ho2ew4Kw5rwSyE1582ZwrU19E36w&__hs=19455.BP%3ADEFAULT.2.0..0.0&__hsi=7219759425496168529&__req=1&__rev=1007275311&__s=s1htvg%3Aeky7pa%3Av30xew&__spin_b=trunk&__spin_r=1007275311&__spin_t=1680981233&__user=0&dpr=1&jazoest=2933&lsd=AVqRbKi8h2cRequest Method: POST
+Status Code: 200 Remote Address: 157.240.9.18:443
+Referrer Policy: origin-when-cross-originaccess-control-allow-credentials: true
+access-control-allow-methods: OPTIONSaccess-control-allow-origin: https://ar-ar.facebook.com
+access-control-expose-headers: X-FB-Debug, X-Loader-Lengthalt-svc: h3=":443"; ma=86400
+cache-control: private, no-cache, no-store, must-revalidatecontent-length: 0
+content-security-policy: default-src data: blob: 'self' https://*.fbsbx.com 'unsafe-inline' *.facebook.com *.fbcdn.net 'unsafe-eval';script-src *.facebook.com *.fbcdn.net *.facebook.net *.google-analytics.com *.google.com 127.0.0.1:* 'unsafe-inline' blob: data: 'self' connect.facebook.net 'unsafe-eval';style-src fonts.googleapis.com *.fbcdn.net data: *.facebook.com 'unsafe-inline';connect-src *.facebook.com facebook.com *.fbcdn.net *.facebook.net wss://*.facebook.com:* wss://*.whatsapp.com:* wss://*.fbcdn.net attachment.fbsbx.com ws://localhost:* blob: *.cdninstagram.com 'self' http://localhost:3103 wss://gateway.facebook.com wss://edge-chat.facebook.com wss://snaptu-d.facebook.com wss://kaios-d.facebook.com/ v.whatsapp.net *.fbsbx.com *.fb.com;font-src data: *.gstatic.com *.facebook.com *.fbcdn.net *.fbsbx.com;img-src *.fbcdn.net *.facebook.com data: https://*.fbsbx.com *.tenor.co media.tenor.com facebook.com *.cdninstagram.com fbsbx.com fbcdn.net *.giphy.com connect.facebook.net *.carriersignal.info blob: android-webview-video-poster: googleads.g.doubleclick.net www.googleadservices.com *.whatsapp.net *.fb.com *.oculuscdn.com;media-src *.cdninstagram.com blob: *.fbcdn.net *.fbsbx.com www.facebook.com *.facebook.com https://*.giphy.com data:;frame-src *.doubleclick.net *.google.com *.facebook.com www.googleadservices.com *.fbsbx.com fbsbx.com data: www.instagram.com *.fbcdn.net https://paywithmybank.com https://sandbox.paywithmybank.com;worker-src blob: *.facebook.com data:;block-all-mixed-content;upgrade-insecure-requests;content-type: text/html; charset="utf-8"
+cross-origin-embedder-policy-report-only: require-corp;report-to="coep_report"cross-origin-opener-policy: same-origin-allow-popups
+date: Sat, 08 Apr 2023 19:13:56 GMTdocument-policy: force-load-at-top
+expires: Sat, 01 Jan 2000 00:00:00 GMTorigin-agent-cluster: ?0
+pragma: no-cachepriority: u=1,i
+report-to: {"max_age":86400,"endpoints":[{"url":"https:\/\/www.facebook.com\/browser_reporting\/?minimize=0"}],"group":"coep_report"}strict-transport-security: max-age=15552000; preload
+vary: Originx-content-type-options: nosniff
+x-fb-debug: kZ9xW13TVHlK1g3fNn9aovQrvMJon8lUYS8JIf4JPIUthSCXGd9WPktubyfC9V1Mfsj6qEyvqVZCr2zc5ljJ1w==x-fb-rlafr: 0
+x-frame-options: DENYx-xss-protection: 0
+:authority: ar-ar.facebook.com:method: POST
+:path: /ajax/bz?__a=1&__ccg=EXCELLENT&__comet_req=0&__dyn=7xe6E5aQ1PyUbFuC1swgE98nwgU29zEdEc8uwdK0lW4o3Bw5VCwjE3awbG782Cw8G0um4o5-0me2218w5uw5Uwdq0Ho2ew4Kw5rwSyE1582ZwrU19E36w&__hs=19455.BP%3ADEFAULT.2.0..0.0&__hsi=7219759425496168529&__req=1&__rev=1007275311&__s=s1htvg%3Aeky7pa%3Av30xew&__spin_b=trunk&__spin_r=1007275311&__spin_t=1680981233&__user=0&dpr=1&jazoest=2933&lsd=AVqRbKi8h2c:scheme: https
+accept: */*accept-encoding: gzip, deflate, br
+accept-language: ar,en-US;q=0.9,en;q=0.8content-length: 2495
+content-type: multipart/form-data; boundary=----WebKitFormBoundaryWNje2P59r1iKoayMcookie: wd=811x625; sb=6bwxZPLAVoiPTtMhVPAkHjBl; datr=6bwxZAG_12-aVlK5iCoqvyl5; fr=0rbBcBBrdBc5Ws8L2..BkMbzp.u2.AAA.0.0.BkMbzx.AWXt5ATxi2c
+origin: https://ar-ar.facebook.comreferer: https://ar-ar.facebook.com/login/?privacy_mutation_token=eyJ0eXBlIjowLCJjcmVhdGlvbl90aW1lIjoxNjgwOTgxMjI1LCJjYWxsc2l0ZV9pZCI6MzgxMjI5MDc5NTc1OTQ2fQ%3D%3D
+sec-ch-ua: "Google
+Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"sec-ch-ua-mobile: ?0
+sec-ch-ua-platform: "Windows"sec-fetch-dest: empty
+sec-fetch-mode: corssec-fetch-site: same-origin
+user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36
--- README.md
+++
@@ -1,1 +0,0 @@
-# abud_alhalbi
